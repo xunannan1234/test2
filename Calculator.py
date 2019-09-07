0196a7d38c70a05211b414f607b08b35dd04fde6
xunannan1234 <53201174+xunannan1234@users.noreply.github.com>
2019-09-07 17:40:36 +0800

    python编写的计算器
    这个完成一个小实验，用python编写一个计算机工资的计算器，比较简单

---
+++ Calculator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("yufa,error")
+        exit()
+    try:
+        income=int(sys.argv[1])
+    except ValueError:
+        print("yufa ,error")
+        exit()
+
+        value =  income-3500
+        if value <= 0:
+            result=0
+        elif 0< value <=1500:
+            result = value*0.03-0
+        elif 1500 < result <= 4500:
+            result = value*0.1-105
+        elif 4500 < result <= 9000:
+            result = value*0.2-555
+        elif 9000 < value <= 35000:
+            result = value*0.25-1005
+        elif 35000 < value <=55000:
+            result = value*0.3-2755
+        elif 55000 < value <= 80000:
+            result = value*0.35-5505
+        else:
+            result = value*0.45-13505
+
+            print('{:.2f}'.format(result))
+
+       if __name__ == '__main__':
+                main()
